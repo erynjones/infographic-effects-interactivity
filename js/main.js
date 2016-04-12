908e8d4b0d42947fabe991a4030f1e7146938c0e
erynjones <eryn.jones16@hotmail.com>
2016-04-12 16:37:59 -0400

    added animation to banner

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $body = $('.banner');
+var $heart;
+var width = $body.outerWidth();
+var height = $body.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $heart = $('<div>');
+  $heart.addClass('heart');
+  $heart.css({
+    top: Math.random() * height,
+    left: Math.random() * width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
+  });
+  $body.append($heart);
+}
